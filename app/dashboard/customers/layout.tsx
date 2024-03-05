2d0d0adcb6efce939ae3046f9827dc7031c6e391
Oliver Sokol <oliver.sokol@nhi.no>
2024-03-05 12:06:04 +0100

    first commit

---
+++ app/dashboard/customers/layout.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Layout({children}: {children: React.ReactNode}){
+
+    return (<div><p>help</p><div>{children}</div></div>);
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Inter, Lusitana, Lustria} from "next/font/google";
+
+
+export const inter = Inter({subsets:['latin']});
+
+export const lustria = Lustria({weight: ["400"],  subsets:["latin"]});
+
+export const lusitana = Lusitana({weight: ["700"], subsets:["latin"]});
